c4d8d64cdf20e3b166192b9c6337f84d3ec2091b
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2019-08-14 21:33:28 +0800

    Add files via upload

---
+++ apptest.py
@@ -0,0 +1,128 @@
+from test import Ui_CamShow
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow,QFileDialog
+from PyQt5.QtCore import QTimer,QCoreApplication
+from PyQt5.QtGui import QPixmap
+import cv2
+import qimage2ndarray
+import time
+
+class CamShow(QMainWindow,Ui_CamShow):
+    def __init__(self,parent=None):
+        super(CamShow,self).__init__(parent)
+        self.setupUi(self)
+        self.PrepWidgets()
+        self.PrepParameters()
+        self.CallBackFunctions()
+        self.Timer = QTimer()
+        self.Timer.timeout.connect(self.TimerOutFun)
+        self.stopbt = 0
+
+    #控件初始化
+    def PrepWidgets(self):
+        self.PrepCamera()
+        self.StopBt.setEnabled(False)
+        self.RecordBt.setEnabled(False)
+
+    def PrepCamera(self):
+        self.camera=cv2.VideoCapture(0)
+  
+
+    def PrepParameters(self):
+        self.RecordFlag=0
+        self.RecordPath='D:/Python/PyQt/'
+        self.FilePathLE.setText(self.RecordPath)
+        self.Image_num=0
+        
+
+    def CallBackFunctions(self):
+        self.FilePathBt.clicked.connect(self.SetFilePath)
+        self.ShowBt.clicked.connect(self.StartCamera)
+        self.StopBt.clicked.connect(self.StopCamera)
+        self.RecordBt.clicked.connect(self.RecordCamera)
+        self.ExitBt.clicked.connect(self.ExitApp)
+        
+
+    def StartCamera(self):
+        self.ShowBt.setEnabled(False)
+        self.StopBt.setEnabled(True)
+        self.RecordBt.setEnabled(True)
+        self.RecordBt.setText('Record')
+        self.Timer.start(1)
+        self.timelb=time.clock()
+
+    def StopCamera(self):
+        if self.StopBt.text() == 'Stop':
+            self.StopBt.setText('Continue')
+            #self.stopbt = 0
+            self.RecordBt.setText('Save Pic')
+            self.Timer.stop()
+        else:
+            self.StopBt.setText('Stop')
+            #self.stopbt = 1
+            self.RecordBt.setText('Record')
+            self.Timer.start(1)
+
+
+    def TimerOutFun(self):
+        success,self.img=self.camera.read()
+        if success:   
+            #self.Image = self.ColorAdjust(img)
+            
+            self.DispImg()            
+            #self.Image_num+=1
+            if self.RecordFlag:
+                self.video_writer.write(self.img)
+                       
+    
+    def DispImg(self):
+        CVimg = cv2.cvtColor(self.img,cv2.COLOR_BGR2RGB)
+        qimg = qimage2ndarray.array2qimage(CVimg)
+        self.DispLb.setPixmap(QPixmap(qimg))
+        self.DispLb.show()
+
+    
+    def SetFilePath(self):
+        dirname = QFileDialog.getExistingDirectory(self, "瀏覽", '.')
+        if dirname:
+
+            self.FilePathLE.setText(dirname)
+            self.RecordPath=dirname+'/'
+
+    def RecordCamera(self):
+        tag=self.RecordBt.text()
+        if tag=='Save Pic':
+            image_name=self.RecordPath+'self.image'+time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))+'.jpg'
+            print(image_name)
+            cv2.imwrite(image_name,self.img)
+            
+        elif tag =='Record':
+            self.RecordBt.setText('Stop')
+            video_name = self.RecordPath + 'video' + time.strftime('%Y%m%d%H%M%S',time.localtime(time.time())) + '.avi'
+            size = (self.img.shape[1],self.img.shape[0])
+            fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+            self.video_writer = cv2.VideoWriter(video_name, fourcc,self.camera.get(5), size)
+            self.RecordFlag=1
+            self.StopBt.setEnabled(False)
+            self.ExitBt.setEnabled(False)
+
+        elif tag == 'Stop':
+            self.RecordBt.setText('Record')
+            self.video_writer.release()
+            self.RecordFlag = 0
+            self.StopBt.setEnabled(True)
+            self.ExitBt.setEnabled(True)
+
+    def ExitApp(self):
+        self.Timer.Stop()
+        self.camera.release()
+        QCoreApplication.quit()
+        
+    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui=CamShow()
+    ui.show()
+    sys.exit(app.exec_())
+    print(self.StopBt.setText())
